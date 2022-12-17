b83c10ab49ec53e78df7c4a7145ef4511855d2ed
=Gorkem <=gorkemtore1@gmail.com>
2022-12-17 22:09:17 +0300

    HipotenusVeAlan try exception eklendi

--- HipotenusVeAlan/src/Main.java
+++ HipotenusVeAlan/src/Main.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		
-		System.out.print("1. Dik kenarı giriniz : ");
-		int k1 = input.nextInt();
-		
-		System.out.print("2. Dik kenarı giriniz : ");
-		int k2 = input.nextInt();
-		
-		//a^2 + b^2 = c^2
-		int calcHypotenuse = (int) Math.sqrt(k1*k1 + k2*k2);
-		
-		System.out.println("Hipotenüs : "+calcHypotenuse);
-		
-		
-		int calcAreaOfTriangle = k1*k2/2;
-		System.out.println("Alan : "+calcAreaOfTriangle);
+		try (Scanner input = new Scanner(System.in)) {
+			System.out.print("1. Dik kenarı giriniz : ");
+			int k1 = input.nextInt();
+			
+			System.out.print("2. Dik kenarı giriniz : ");
+			int k2 = input.nextInt();
+			
+			//a^2 + b^2 = c^2
+			int calcHypotenuse = (int) Math.sqrt(k1*k1 + k2*k2);
+			
+			System.out.println("Hipotenüs : "+calcHypotenuse);
+			
+			
+			int calcAreaOfTriangle = k1*k2/2;
+			System.out.println("Alan : "+calcAreaOfTriangle);
+		}
 
 	}
 
