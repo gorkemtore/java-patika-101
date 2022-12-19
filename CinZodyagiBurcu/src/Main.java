a3fcc83a21308721c382ce991f4eda86f551842c
Gorkem <gorkemtore1@gmail.com>
2022-12-19 16:43:40 +0300

    CinZoyagiBurcu eklendi

---
+++ CinZodyagiBurcu/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String[] burclar = {"Maymun","Horoz","Köpek","Domuz","Fare","Öküz",
+				"Kaplan","Tavşan","Enderha","Yılan","At","Koyun"};
+		
+		System.out.println("Doğum yılınızı giriniz : ");
+		short dogumYili = input.nextShort();
+		
+		int kalan = dogumYili % 12;
+		
+		for (int i=0 ; i<12 ; i++) {
+			if (kalan==i) {
+				System.out.println("Burcunuz : "+burclar[i]);
+			}
+		}
+		System.out.println(2002%12);
+		
+
+	}
+
+}
