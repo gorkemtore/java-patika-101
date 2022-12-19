47cfb6125e1d9e2999a833bc262382600ca14f9e
Gorkem <gorkemtore1@gmail.com>
2022-12-19 19:48:52 +0300

    burclar dizisi gucenllendi.

--- CinZodyagiBurcu/src/Main.java
+++ CinZodyagiBurcu/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		String[] burclar = {"Maymun","Horoz","Köpek","Domuz","Fare","Öküz",
-				"Kaplan","Tavşan","Enderha","Yılan","At","Koyun"};
+				"Kaplan","Tavşan","Ejderha","Yılan","At","Koyun"};
 		
 		System.out.println("Doğum yılınızı giriniz : ");
 		short dogumYili = input.nextShort();
