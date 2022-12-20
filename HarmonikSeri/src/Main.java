cfbe99576098294bd47e2ab0bc1fc46c2fdea159
Gorkem <gorkemtore1@gmail.com>
2022-12-20 19:51:52 +0300

    HarmonikSeri eklendi.

--- HarmonikSeri/src/Main.java
+++ HarmonikSeri/src/Main.java
@@ -1,6 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        System.out.println("Hello world!");
-    }
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("n sayısı giriniz: ");
+		int n = input.nextInt();
+		
+		double toplam=0.0;
+		
+		for (double i =1; i <= n; i++) {
+			toplam += (1/i);	
+		}
+		System.out.println("Hesaplanan : "+toplam);
+	}
+
 }
+
